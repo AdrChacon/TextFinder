5cd42afb6c26f1dfb2d4e20f8a495ff0559d7129
Esteban Morales <eamu11@hotmail.com>
2019-10-09 12:53:58 -0600

    intefaz

---
+++ TextFinder/src/Screen.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Screen  {
+
+}
