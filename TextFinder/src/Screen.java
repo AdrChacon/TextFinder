ced93ee74a5172b3ad8c70742ebd7a9380bb094a
Esteban Morales <eamu11@hotmail.com>
2019-10-09 13:01:25 -0600

    intefaz 2.0, creacion de ventana y boton

--- TextFinder/src/Screen.java
+++ TextFinder/src/Screen.java
@@ -6,6 +6,39 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-public class Screen  {
+public class Screen extends JFrame implements ActionListener {
 
-}
+    public Screen(){
+        //Para poder cerrar la ventana
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        //Se agrega un layout
+        setLayout( new BorderLayout() );
+
+        //Se crea el editor de texto y se agrega a un scroll
+        txp = new JTextPane();
+        JScrollPane jsp = new JScrollPane();
+        jsp.setViewportView( txp );
+
+        add( jsp, BorderLayout.CENTER );
+
+        //Se crea un boton para abrir el archivo
+        JButton btn = new JButton( "Abrir" );
+        btn.addActionListener( this );
+        //btn.setIcon( new ImageIcon( getClass().getResource( "Abrir.png" ) ) );
+
+        add( btn, BorderLayout.NORTH );
+
+        //Tamaño de la ventana
+        setSize( 500, 500 );
+
+        //Esto sirve para centrar la ventana
+        setLocationRelativeTo( null );
+
+        //Hacemos visible la ventana
+        setVisible( true );
+    }
+
+
+
+
