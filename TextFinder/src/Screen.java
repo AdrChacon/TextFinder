53b24b4bf58610ebcdbe2ed2f2bcc09caba3a6aa
Esteban Morales <eamu11@hotmail.com>
2019-10-09 13:02:46 -0600

    intefaz 2.0, Action Performed

--- TextFinder/src/Screen.java
+++ TextFinder/src/Screen.java
@@ -39,6 +39,40 @@ public class Screen extends JFrame implements ActionListener {
         setVisible( true );
     }
 
+    //------------------------------Action Performed-------------------------------//
+    public void actionPerformed( ActionEvent e ){
+        JButton btn = (JButton)e.getSource();
+        if( btn.getText().equals( "Abrir" ) )
+        {
+            if( abrirArchivo == null ) abrirArchivo = new JFileChooser();
+            //Con esto solamente podamos abrir archivos
+            abrirArchivo.setFileSelectionMode( JFileChooser.FILES_ONLY );
 
+            int seleccion = abrirArchivo.showOpenDialog( this );
+
+            if( seleccion == JFileChooser.APPROVE_OPTION )
+            {
+                File f = abrirArchivo.getSelectedFile();
+                try
+                {
+                    String nombre = f.getName();
+                    String path = f.getAbsolutePath();
+                    String contenido = getArchivo( path );
+                    //Colocamos en el titulo de la aplicacion el
+                    //nombre del archivo
+                    this.setTitle( nombre );
+
+                    //En el editor de texto colocamos su contenido
+                    txp.setText( contenido );
+
+                }catch( Exception exp){}
+            }
+        }
+    }
+    //-----------------------------------------------------------------------------//
+
+
+
+}
 
 
