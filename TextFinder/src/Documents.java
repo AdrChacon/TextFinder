623369b3f901e2a64a4abf93f3c8ca7580e15eac
Esteban Morales <eamu11@hotmail.com>
2019-10-25 00:29:37 -0600

    Atributos de clase documents

--- TextFinder/src/Documents.java
+++ TextFinder/src/Documents.java
@@ -1,2 +1,10 @@
 public class Documents {
+    private String text;
+    private String name;
+    private String size;
+    private String date;
+    private int realSize;
+
+    protected Documents next = null;
+    protected Documents prev = null;
 }
