5ceff1fa5e26ca8f7f53661ca454246846759899
Esteban Morales <eamu11@hotmail.com>
2019-10-24 22:39:15 -0600

    Creacion ventana principal en el main

--- TextFinder/src/Main.java
+++ TextFinder/src/Main.java
@@ -1,2 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Text Finder");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
