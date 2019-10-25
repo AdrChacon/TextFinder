1227bb0b8061f8ff3b3b162e49a5eaefa3cd7367
Esteban Morales <eamu11@hotmail.com>
2019-10-25 00:39:10 -0600

    Completacion de clase pdf reader

--- TextFinder/src/PdfReader.java
+++ TextFinder/src/PdfReader.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 public class PdfReader {
+
+    public static String[] pdfReader (String path) throws IOException {
+        PDDocument document = PDDocument.load(new File(path));
+        if (!document.isEncrypted()) {
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(true);
+            PDFTextStripper tStripper = new PDFTextStripper();
+            String pdfFileInText = tStripper.getText(document);
+            String[] lines = pdfFileInText.split("\\r?\\n");
+            return lines;
+        }
+        return null;
+    }
 }
