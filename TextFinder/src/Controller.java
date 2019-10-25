dba81aee37d6b5701db83893a761eddcdd4ef71e
Esteban Morales <eamu11@hotmail.com>
2019-10-24 22:47:57 -0600

    botones en clase controller

--- TextFinder/src/Controller.java
+++ TextFinder/src/Controller.java
@@ -1,2 +1,47 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Controller {
-}
+    public class Controller {
+
+        @FXML
+        Button indexBtn;
+
+        @FXML
+        Button refreshBtn;
+
+        @FXML
+        Button addBtn;
+
+        @FXML
+        Button deleteBtn;
+
+        @FXML
+        Button searchBtn;
+
+        @FXML
+        TextField inputField;
+
+        @FXML
+        ListView textPane;
+
+        @FXML
+        ListView namePane;
+
+        @FXML
+        ListView sizePane;
+
+        @FXML
+        ListView datePane;
+
+        @FXML
+        ListView libraryListView;
+
+
+        @FXML
+        TableView<Documents> resultsTable;
+
+    }
