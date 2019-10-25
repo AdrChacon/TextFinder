44b1d1b0c40727186de3ff2f9743e39347bf8a74
Esteban Morales <eamu11@hotmail.com>
2019-10-25 00:10:10 -0600

    Modificacion al radixsort

--- TextFinder/src/RadixSort.java
+++ TextFinder/src/RadixSort.java
@@ -1,9 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+
 public class RadixSort {
     // A utility function to get maximum value in arr[]
-    static int getMax(int arr[], int n)
-    {
+    static int getMax(int arr[], int n) {
         int mx = arr[0];
         for (int i = 1; i < n; i++)
             if (arr[i] > mx)
@@ -13,8 +13,7 @@ public class RadixSort {
 
     // A function to do counting sort of arr[] according to
     // the digit represented by exp.
-    static void countSort(int arr[], int n, int exp)
-    {
+    public static void countSort(int arr[], int n, int exp) {
         int output[] = new int[n]; // output array
         int i;
         int count[] = new int[10];
@@ -30,8 +29,7 @@ public class RadixSort {
             count[i] += count[i - 1];
 
         // Build the output array
-        for (i = n - 1; i >= 0; i--)
-        {
+        for (i = n - 1; i >= 0; i--) {
             output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
             count[ (arr[i]/exp)%10 ]--;
         }
@@ -44,32 +42,36 @@ public class RadixSort {
 
     // The main function to that sorts arr[] of size n using
     // Radix Sort
-    static void radixsort(int arr[], int n)
-    {
+    public static int[] radixsort(int arr[], int n) {
         // Find the maximum number to know number of digits
         int m = getMax(arr, n);
-
         // Do counting sort for every digit. Note that instead
         // of passing digit number, exp is passed. exp is 10^i
         // where i is current digit number
-        for (int exp = 1; m/exp > 0; exp *= 10)
+        for (int exp = 1; m/exp > 0; exp *= 10){
             countSort(arr, n, exp);
+        }
+        return arr;
     }
 
-    // A utility function to print an array
-    static void print(int arr[], int n)
-    {
-        for (int i=0; i<n; i++)
-            System.out.print(arr[i]+" ");
+    public static int[] toIntArray(DocumentsDoublyLinkedList dl){
+        int[] result = new int[dl.getLength()-1];
+        for(int i = 0; i < dl.getLength(); i++){
+            result[i] = dl.get(i).getRealSize();
+        }
+        return result;
     }
 
-
-    /*Driver function to check for above function*/
-    public static void main (String[] args)
-    {
-        int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
-        int n = arr.length;
-        radixsort(arr, n);
-        print(arr, n);
+    public static DocumentsDoublyLinkedList backToDoublyList(int[] result, DocumentsDoublyLinkedList dl) throws MalformedURLException {
+        DocumentsDoublyLinkedList tmpList = dl;
+        dl.clearList();
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; j < tmpList.getLength(); j++){
+                if (result[i] == tmpList.get(i).getRealSize()){
+                    dl.addLast(tmpList.get(i));
+                }
+            }
+        }
+        return dl;
     }
 }
