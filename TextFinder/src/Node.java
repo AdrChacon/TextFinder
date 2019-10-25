ab2e2652ac343c58e0a1ea8d5135429e01286424
Esteban Morales <eamu11@hotmail.com>
2019-10-25 00:19:06 -0600

    completacion de clase node

--- TextFinder/src/Node.java
+++ TextFinder/src/Node.java
@@ -1,2 +1,40 @@
 public class Node {
+
+    private Node left;
+
+    private Node rigth;
+
+    private WordOcurrences wordOcurrences;
+
+    private String word;
+
+    public Node(String word){
+        this.word=word;
+        wordOcurrences=new WordOcurrences();
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRigth() {
+        return rigth;
+    }
+
+    public void setRigth(Node rigth) {
+        this.rigth = rigth;
+    }
+
+    public WordOcurrences getWordOcurrences() {
+        return wordOcurrences;
+    }
+
+    public String getWord() {
+        return word;
+    }
 }
+
